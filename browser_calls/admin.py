5b25e0a3d50745bc7ab9b383f922ace02cf39e99
Samuel Mendes <samuel.mendes.new@gmail.com>
2017-11-29 14:39:43 +0100

    [Auto] Upgrade to twilio 6.9.0

--- browser_calls/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import SupportTicket
-
-
-class SupportTicketAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'phone_number')
-
-admin.site.register(SupportTicket, SupportTicketAdmin)
